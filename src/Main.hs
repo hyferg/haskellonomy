7dddc53d1a170aab78272a4eb86d79a00f79c465
Cameron Ferguson <cameron@bonsaidesk.com>
2023-06-25 14:33:39 -0600

    bit of info in the main file

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,14 @@ module Main where
 
 import LiftIndex
 import Format
+import System.Directory (canonicalizePath)
 
+path = "./data-out/liftIndex.txt"
+theLift = lifted
+
+-- write lift to file and log to console that we're done
 main :: IO ()
 main = do
-  writeFile "./data-out/liftIndex.txt" (showcsv lifted)
+  writeFile path (showcsv theLift)
+  fullPath <- canonicalizePath path
+  putStrLn ("Wrote lift to file: " ++ fullPath)
